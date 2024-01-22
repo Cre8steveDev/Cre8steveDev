d4bd28e63953c67fae28f2917262634f2c4f3d01
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-01-22 16:28:59 +0100

    Moved Browser Router Component to main.jsx > subject to review

--- src/GlassMorphism/GlassMorphism.jsx
+++ src/GlassMorphism/GlassMorphism.jsx
@@ -6,7 +6,7 @@ import Portfolio from "./Portfolio.jsx";
 import Resume from "./Resume.jsx";
 import Contact from "./Contact.jsx";
 import ProjectView from "../components/ProjectView.jsx";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 // Additionals will come here
 
@@ -15,27 +15,25 @@ const GlassMorphism = ({ setScrollY }) => {
   // Define ref for container
 
   return (
-    <BrowserRouter>
-      <div className="flex flex-col  items-center relative snap-y snap-mandatory w-full md:w-[90%] lg:max-w-screen-lg overflow-y-scroll">
-        <Header />
-        <Routes>
-          <Route
-            index
-            element={
-              <div className="">
-                <HeroPage setScrollY={setScrollY} />
-                <Skills setScrollY={setScrollY} />
-                <Portfolio setScrollY={setScrollY} />
-                <Resume setScrollY={setScrollY} />
-                <Contact setScrollY={setScrollY} />
-              </div>
-            }
-          />
-          <Route path="project/:param" element={<ProjectView />} />
-          <Route path="*" element={<div>Error</div>} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div className="flex flex-col  items-center relative snap-y snap-mandatory w-full md:w-[90%] lg:max-w-screen-lg overflow-y-scroll">
+      <Header />
+      <Routes>
+        <Route
+          index
+          element={
+            <div className="">
+              <HeroPage setScrollY={setScrollY} />
+              <Skills setScrollY={setScrollY} />
+              <Portfolio setScrollY={setScrollY} />
+              <Resume setScrollY={setScrollY} />
+              <Contact setScrollY={setScrollY} />
+            </div>
+          }
+        />
+        <Route path="project/:param" element={<ProjectView />} />
+        <Route path="*" element={<div>Error</div>} />
+      </Routes>
+    </div>
   );
 };
 
