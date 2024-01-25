250db58afe1e9c692c113adf3159a76eb449a62f
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-01-25 08:08:09 +0100

    Sidebar debugging 002

--- src/App.jsx
+++ src/App.jsx
@@ -47,7 +47,7 @@ function App() {
       {!glassMorphism && <RandomQuote />}
 
       {/* Interactivity */}
-      <div className="  w-[30px] sm:w-[40px] absolute top-[410px] sm:top-32 sm:right-3 right-0 overflow-x-hidden">
+      <div className="  w-[30px] sm:w-[40px] absolute top-[400px] sm:top-32 sm:right-3 right-0 overflow-x-hidden bg-white">
         {/* Scroll Progress Bar */}
 
         {!viewingProduct && (
